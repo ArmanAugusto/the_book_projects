c18798f1b25d4e3248b28e537b61183f48e08773
vagrant <vagrant@server>
2024-03-23 06:59:30 -0700

    initial commit

---
+++ guessing_game/target/debug/deps/guessing_game-e920a0eb71094efb.d
@@ -0,0 +1,5 @@
+/home/vagrant/Projects/the_book_projects/guessing_game/target/debug/deps/libguessing_game-e920a0eb71094efb.rmeta: src/main.rs
+
+/home/vagrant/Projects/the_book_projects/guessing_game/target/debug/deps/guessing_game-e920a0eb71094efb.d: src/main.rs
+
+src/main.rs:
